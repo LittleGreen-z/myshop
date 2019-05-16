bddf0eb5c8b93917158ea6432106d74f3d973aa6
LittleGreen-z <zhao2946@sina.com>
2019-05-16 20:10:41 +0800

    完成搜索条菜单

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,17 +1,33 @@
 <template>
-    <div>
-      首页
+  <div>
+    <div class="search">
+      <div class="search-bar">
+        <icon type="search"></icon>
+        <span>搜索</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: '',
-  data () {
+  name: "",
+  data() {
     return {}
   }
-}
+};
 </script>
 
 <style>
+.search {
+  background-color: #eb4450;
+  padding: 10px;
+}
+.search .search-bar {
+  background-color: white;
+  text-align: center;
+}
+.search .search-bar icon {
+  vertical-align: middle
+}
 </style>
