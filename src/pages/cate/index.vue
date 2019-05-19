1664845906bf7bbb31daed1547e7e5db5d9b077e
LittleGreen-z <zhao2946@sina.com>
2019-05-19 19:42:36 +0800

    完成左侧菜单切换

--- src/pages/cate/index.vue
+++ src/pages/cate/index.vue
@@ -4,11 +4,25 @@
       <search-bar></search-bar>
       <div class="content">
         <div class="left">
-          <div class="menu-item" :class="{active: menu.cat_id == currentId}" :key='menu.cat_id' v-for='menu in list'>
+          <div @click="changeMenu(menu.cat_id)" class="menu-item" :class="{active: menu.cat_id == currentId}" :key='i' v-for='(menu,i) in list'>
             {{menu.cat_name}}
           </div>
         </div>
-        <div class="right"></div>
+        <div class="right">
+          <!-- 第一层遍历 大分类 -->
+          <div v-for='(second,index1) in rightData' :key='index1' class="brand-item">
+            <!-- 二级分类标题 -->
+            <div class="brand-title">{{second.cat_name}}</div>
+            <!-- 二级分类内容 -->
+            <div class="brand-list">
+              <!-- 三级列表的分类 -->
+              <div class="brand" v-for="brand in second.children" :key="brand.cat_id">
+                <img :src="brand.cat_icon" mode="aspectFill">
+                <p>{{brand.cat_name}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 </template>
@@ -21,7 +35,16 @@ export default {
   data () {
     return {
       list: [],
-      currentId: 1
+      currentId: 1,
+    }
+  },
+  computed: {
+    rightData () {
+      let rd =this.list.filter(item => {
+        return item.cat_id == this.currentId
+      })
+      let ret = rd[0].children
+      return ret
     }
   },
   components: {
@@ -34,6 +57,9 @@ export default {
     async queryData () {
       let res = await request('categories')
       this.list = res.data.message
+    },
+    changeMenu (id) {
+      this.currentId = id
     }
   },
 }
