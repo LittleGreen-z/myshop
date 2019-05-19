2bf7e671fdc45187afc95550cc492f57384d1edc
LittleGreen-z <zhao2946@sina.com>
2019-05-19 20:55:58 +0800

    搜索功能基本完成

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -2,18 +2,38 @@
   <div class="search">
       <div class="search-input">
         <icon type='search' size='16'/>
-        <input placeholder="请输入关键字" />
+        <input @input="keywordSearch" v-model="keyword" placeholder="请输入关键字" />
       </div>
-      <button class="cancel">取消</button>
+      <button @click="cancelSearch" v-if="keyword" class="cancel">取消</button>
+      <div  v-if="keyword" class="search-modal">
+        <div v-for="(item,i) in kdList" :key="i" class="search-item">
+          {{item.goods_name}}
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import request from '../../utils/request'
 export default {
   name: '',
   data () {
-    return {}
-  }
+    return {
+      keyword: '',
+      kdList: []
+    }
+  },
+  methods: {
+    cancelSearch () {
+      this.keyword= ''
+    },
+    async keywordSearch () {
+      let ret = await request('goods/qsearch','get',{
+        query: this.keyword
+      })
+      this.kdList = ret.data.message
+    }
+  },
 }
 </script>
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,10 @@
-const request = (path) => {
+const request = (path,method ='GET',data={}) => {
   let url = 'https://www.zhengzhicheng.cn/api/public/v1/'+ path
   return new Promise(function(resolve,reject) {
     mpvue.request({
       url: url,
+      method:method,
+      data: data,
       success: function (res) {
         resolve(res)
       }
